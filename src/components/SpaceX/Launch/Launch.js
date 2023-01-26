b96be3ee500e1b528770fafd23d587dc215ef551
DomkaD <115803411+DomkaD@users.noreply.github.com>
2023-01-26 16:36:44 +0200

    Delete src/components/SpaceX directory

--- src/components/SpaceX/Launch/Launch.js
+++
@@ -1,9 +0,0 @@
-export function Launch({item}) {
-
-    return (
-        <div>
-            <h2>{item.flight_number} - {item.rocket}</h2>
-            <img src={item.links.patch.small}/>
-        </div>
-    );
-}
